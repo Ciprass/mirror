fca7d9b4446b722f909c5c1844e0d336b72349fb
Dorion Y. Chou <Joy.YiZhou@proton.me>
2024-09-17 22:08:53 +0800

    配置项目

---
+++ src/main/java/cipras/mirror_world/MirrorWorld.java
@@ -0,0 +1,16 @@
+package cipras.mirror_world;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MirrorWorld implements ModInitializer {
+	public static final String MOD_ID = "mirror_world";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/cipras/mirror_world/MirrorWorldClient.java
@@ -0,0 +1,10 @@
+package cipras.mirror_world;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MirrorWorldClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
