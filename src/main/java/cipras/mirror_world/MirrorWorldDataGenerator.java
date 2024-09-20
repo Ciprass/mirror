62278423071491c975bf8fb58504c5e9f0f02720
Ciprass <joy.yizhou@hotmail.com>
2024-09-20 13:41:20 +0800

    加入成就

--- src/main/java/cipras/mirror_world/MirrorWorldDataGenerator.java
+++ src/main/java/cipras/mirror_world/MirrorWorldDataGenerator.java
@@ -2,10 +2,11 @@ package cipras.mirror_world;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import cipras.mirror_world.Datagen.AdvancementsProvider;
 
 public class MirrorWorldDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-
+		fabricDataGenerator.createPack().addProvider(AdvancementsProvider::new);
 	}
 }
